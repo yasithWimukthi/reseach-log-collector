b8f9630524fefb53653ad436087a80f120fe8858
yasithwimukthi <wimukthiyasith@gmail.com>
2026-04-16 23:32:57 +0530

    Initial commit

---
+++ src/test/java/com/microservices/logcollector/LogCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.logcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
